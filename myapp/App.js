ae1fbafe0707a980ef4a43a44d238b5dc5d7c5ad
etcheverrypablol <etcheverrypablol@gmail.com>
2022-03-01 09:30:48 -0300

    Using components

--- myapp/App.js
+++ myapp/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginForm from './src/components/LoginForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!!</Text>
       <Text>I'm Pablo</Text>
+      <LoginForm />
       <StatusBar style="auto" />
     </View>
   );
